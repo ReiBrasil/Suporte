8812e16b72b4b51b33c92488b330d50907069cd0
RennanTCS <103700348+ReiBrasil@users.noreply.github.com>
2025-01-10 11:44:41 -0300

    Update Desenvolvedor.js

--- components/Desenvolvedor.js
+++ components/Desenvolvedor.js
@@ -1,22 +1,16 @@
 function handleOptionChange(select) {
     const value = select.value;
     if (value === 'informacoes') {
-        window.location.href = "file:///c%3A/Users/rti/Desktop/Okay/Informa%C3%A7%C3%B5es/Biosphera%20Informa%C3%A7%C3%B5es.html";
         window.location.href = "file:///c%3A/Users/rei-b/OneDrive/Área%20de%20Trabalho/Desenvolvimento/Okay/pages/Biosphera%20Informações.html";
     } if (value === 'inicial') {
-        window.location.href = "file:///c%3A/Users/rti/Desktop/Okay/Inicial/Biosphera%20Inicial.html";
         window.location.href = "file:///c%3A/Users/rei-b/OneDrive/Área%20de%20Trabalho/Desenvolvimento/Okay/pages/Biosphera%20Inicial.html";
     } else if (value === 'downloads') {
-        window.location.href = "file:///c%3A/Users/rti/Desktop/Okay/Download/Biosphera%20Download.html";
         window.location.href = "file:///c%3A/Users/rei-b/OneDrive/Área%20de%20Trabalho/Desenvolvimento/Okay/pages/Biosphera%20Download.html";
     } else if (value === 'integraçoes') {
-    window.location.href = "file:///c%3A/Users/rti/Desktop/Okay/Integra%C3%A7%C3%B5es/Biosphera%20Integra%C3%A7%C3%B5es.html";
     window.location.href = "file:///c%3A/Users/rei-b/OneDrive/Área%20de%20Trabalho/Desenvolvimento/Okay/pages/Biosphera%20Integrações.html";
     } else if (value === 'desenvolvedores') {
-    window.location.href = "file:///c%3A/Users/rti/Desktop/Okay/Desenvolvedores/Biosphera%20Desenvolvedores.html";
     window.location.href = "file:///c%3A/Users/rei-b/OneDrive/Área%20de%20Trabalho/Desenvolvimento/Okay/pages/Biosphera%20Desenvolvedores.html";
     } else if (value === 'suporte') {
-    window.location.href = "file:///c%3A/Users/rti/Desktop/Okay/Suporte/Biosphera%20Suporte.html";
     window.location.href = "file:///c%3A/Users/rei-b/OneDrive/Área%20de%20Trabalho/Desenvolvimento/Okay/pages/Biosphera%20Suporte.html";
 }
 }
